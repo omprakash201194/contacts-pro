1665c7a5a82c28ffb3a9625b6f3154b72c42af4a
Omprakash Gautam <omprakash201194@gmail.com>
2021-07-02 19:45:43 +0530

    Added todo's

--- src/main/java/com/omprakashgautam/app/contactspro/ContactsProApplication.java
+++ src/main/java/com/omprakashgautam/app/contactspro/ContactsProApplication.java
@@ -10,4 +10,23 @@ public class ContactsProApplication {
 		SpringApplication.run(ContactsProApplication.class, args);
 	}
 
+
+	//TODO 1. Setup SQL scripts to add tables - for postgres
+	//TODO 2. Add repository layer
+	//TODO 3. Add Service layer
+	//TODO 4: Add Rest layer
+	//TODO 5: Add swagger to document api's
+	//TODO 6: Configure Logging
+	//TODO 7: Add hateos to rest api
+	//TODO 8: Add global exception handler
+	//TODO 9: Add unit test cases for each layer
+	//TODO 10: Add integration test cases
+	//TODO 11: Integrate Liquibase for tracking sql schema changes
+	//TODO 12: Add jenkins for CI / CD
+	//TODO 13: Add dockerization or kubernetes
+	//TODO 14: Add react front end
+	//TODO 15: Add android app for the same
+	//TODO 16: Add caching - redis
+	//TODO 17: Add elastic search for indexing
+	//TODO 18: Add spring boot profiles for dev, prod, test
 }
