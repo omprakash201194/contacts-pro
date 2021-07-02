f0aefa586d13d40d8ca943199efaf6dfe1edcb35
Omprakash Gautam <omprakash201194@gmail.com>
2021-07-02 18:40:03 +0530

    Bootsrapped project

---
+++ src/main/java/com/omprakashgautam/app/contactspro/ContactsProApplication.java
@@ -0,0 +1,13 @@
+package com.omprakashgautam.app.contactspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omprakashgautam/app/contactspro/ContactsProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omprakashgautam.app.contactspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
